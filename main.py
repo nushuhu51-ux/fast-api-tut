dd54fdf88c9576ef9bf0496d4387189e443c3176
Samuel <nushuhu51@gmail.com>
2026-07-25 00:54:13 +0300

    basic fastAPI

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return{ "Hello":"World" }
+    return{ "Message":"Hello Samuel" }
+
+@app.get("/greet")
+def greet():
+    return{"Message":"Hello Teshale"}
+
+@app.get("/greet{name}")
+def greet_name(name:str, age: Optional[int]=None):
+    return {"Message":f"Hello {name} are good? you are {age} years old."}
